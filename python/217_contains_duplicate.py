126354f7f6be81f3559ede6d17fe5b119e07386a
Kabilan Thennarasu <kabilan.psgtech@gmail.com>
2026-01-28 22:07:46 -0800

    217. Contains Duplicate

---
+++ python/217_contains_duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        map = {}
+        for num in nums:
+            if num in map:
+                return True
+            else:
+                map[num] = 1
+        return False
+        
