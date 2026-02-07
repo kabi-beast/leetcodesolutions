c9b6c48b2f7a5f6ee9336e5d7805bd1b87aba36f
Kabilan Thennarasu <kabilan.psgtech@gmail.com>
2026-02-06 21:48:30 -0800

    1550. Three Consecutive Odds Solution

---
+++ python/1550_three_consecutive_odds.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odd = 0
+        for num in arr:
+            if num % 2 != 0:
+                odd += 1
+                if odd == 3:
+                    return True
+            else:
+                odd = 0
+        return False
+        
